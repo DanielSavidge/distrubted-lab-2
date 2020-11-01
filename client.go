5e87c7637fc7b2470bca9fd3c2e4535940e05e5e
Daniel Savidge <55741686+DanielSavidge@users.noreply.github.com>
2020-11-01 23:47:26 +0000

    Update client.go

--- client.go
+++ client.go
@@ -12,18 +12,22 @@ func read(conn *net.Conn) {
 	//TODO In a continuous loop, read a message from the server and display it.
 	reader := bufio.NewReader(*conn)
 	for {
-		msg, _ := reader.ReadString('\n')
-		fmt.Println(msg)
+		line, _ := reader.ReadString('\n')
+		fmt.Println(":: ", line)
 	}
 }
 
 func write(conn *net.Conn) {
 	//TODO Continually get input from the user and send messages to the server.
-	stdin := bufio.NewReader(os.Stdin)
-	fmt.Println("Enter message to send to the server: ")
+	reader := bufio.NewReader(os.Stdin)
 	for {
-		msg, _ := stdin.ReadString('\n')
-		fmt.Fprintln(*conn, msg)
+		fmt.Println("Enter text:")
+		text, _ := reader.ReadString('\n')
+		if text == "/quit\n" {
+			break
+		} else {
+			fmt.Fprintf(*conn, text)
+		}
 	}
 }
 
@@ -31,10 +35,12 @@ func main() {
 	// Get the server address and port from the commandline arguments.
 	addrPtr := flag.String("ip", "127.0.0.1:8030", "IP:port string to connect to")
 	flag.Parse()
+
 	//TODO Try to connect to the server
 	conn, _ := net.Dial("tcp", *addrPtr)
-	//TODO Start asynchronously reading and displaying messages
+
 	go read(&conn)
-	//TODO Start getting and sending user messages.
 	write(&conn)
+	//TODO Start asynchronously reading and displaying messages
+	//TODO Start getting and sending user messages.
 }
